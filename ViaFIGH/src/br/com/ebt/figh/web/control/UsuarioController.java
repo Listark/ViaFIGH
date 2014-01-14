17a2a1d9c4b98124e0cb1e8c1628d31521f86b5a
olbruno <olbruno@OLBRUNO4.americas.hpqcorp.net>
2014-01-14 11:07:57 -0200

    Reestruturação.

--- ViaFIGH/src/br/com/ebt/figh/web/control/UsuarioController.java
+++ ViaFIGH/src/br/com/ebt/figh/web/control/UsuarioController.java
@@ -1,5 +1,6 @@
 package br.com.ebt.figh.web.control;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,8 +10,10 @@ import javax.faces.bean.ViewScoped;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import br.com.ebt.figh.model.Fornecedor;
 import br.com.ebt.figh.model.Perfil;
 import br.com.ebt.figh.model.Usuario;
+import br.com.ebt.figh.services.FornecedorService;
 import br.com.ebt.figh.services.UsuarioService;
 import br.com.ebt.figh.web.util.FacesUtil;
 
@@ -18,10 +21,15 @@ import br.com.ebt.figh.web.util.FacesUtil;
 @ViewScoped
 @ManagedBean(name = "usuarioController")
 public class UsuarioController {
+	private static final String INSERT_SUCCESS = "Usuário cadastrado com sucesso!";
+	private static final String DELETE_SUCCESS = "Usuário excluído com sucesso!";
+	private static final String EDIT_SUCCESS   = "Usuário alterado com sucesso";
 	private Usuario usuario;
 	private List<Usuario> usuarios;
 	private List<Perfil> perfis;
+	private Perfil perfil;
 	private UsuarioService servico;
+	private FornecedorService servicoForn;
 	private Long idPerfil;
 	private Long idFornecedor;
 	private boolean edicao;
@@ -35,6 +43,7 @@ public class UsuarioController {
 	
 	public void cadastrar() {
 		edicao = false;
+		hideComboForn = true;
 		usuario = null;
 		idPerfil = null;
 		idFornecedor = null;
@@ -48,13 +57,59 @@ public class UsuarioController {
 		
 		if(usuario == null || usuario.getId() != null) {
 			idPerfil = usuario.getPerfis().get(0).getId();
+			avaliarPerfil(idPerfil);
 			
 			if(usuario.getFornecedor() != null)
 				idFornecedor = usuario.getFornecedor().getId();
+				
 		}
 		
 		FacesUtil.executarScript("PF('wUsuario').show()");
 		
+	}
+	
+	public void salvar() {
+		List<Perfil> perfis = new ArrayList<Perfil>();
+		perfis.add(servico.buscarPerfil(idPerfil));
+		usuario.setPerfis(perfis);
+		
+		if(idPerfil == 2) {
+			Fornecedor f = servicoForn.getById(idFornecedor);
+			usuario.setFornecedor(f);
+		}
+		
+		servico.salvarUsuario(usuario);
+		
+		if(edicao) {
+			usuarios.set(usuarios.indexOf(usuario), usuario);
+			FacesUtil.exibirMensagemSucesso(EDIT_SUCCESS);
+		} else {
+			usuarios.add(usuario);
+			FacesUtil.exibirMensagemSucesso(INSERT_SUCCESS);
+		}
+	}
+	
+	public void excluir() {
+		servico.excluirUsuario(usuario);
+		usuarios.remove(usuarios.indexOf(usuario));
+		
+		FacesUtil.executarScript("PF('wExclusao').hide()");
+		FacesUtil.exibirMensagemSucesso(DELETE_SUCCESS);
+	}
+	
+	private void avaliarPerfil(Long id) {
+		if(id == null)
+			hideComboForn = true;
+		
+		if(id == 1) {
+			hideComboForn = true;
+			idFornecedor = null;
+		} else
+			hideComboForn = false;
+	}
+	
+	public void showHideComboForn() {
+		avaliarPerfil(idPerfil);
 	}
 	
 	public Usuario getUsuario() {
@@ -122,6 +177,25 @@ public class UsuarioController {
 	
 	public void setHideComboForn(boolean hideComboForn) {
 		this.hideComboForn = hideComboForn;
+	}
+	
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+	
+	public Perfil getPerfil() {
+		if(perfil == null)
+			perfil = new Perfil();
+		return perfil;
+	}
+	
+	@Autowired
+	public void setServicoForn(FornecedorService servicoForn) {
+		this.servicoForn = servicoForn;
+	}
+	
+	public FornecedorService getServicoForn() {
+		return servicoForn;
 	}
 	
 }
